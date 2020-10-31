b7a8d4b4045e1bd1e8ed32777a50e6416bad01ce
victor atencio <victorj.atencio@gmail.com>
2020-10-31 18:41:53 -0400

    Started app

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDVwWUapJJALtBidGVug5N9uweyhws6ei0",
+  authDomain: "bli-remake.firebaseapp.com",
+  databaseURL: "https://bli-remake.firebaseio.com",
+  projectId: "bli-remake",
+  storageBucket: "bli-remake.appspot.com",
+  messagingSenderId: "196121998652",
+  appId: "1:196121998652:web:8c3bc68b92044a4f8b1a39",
+  measurementId: "G-HJS6WCFWMP"
+};
